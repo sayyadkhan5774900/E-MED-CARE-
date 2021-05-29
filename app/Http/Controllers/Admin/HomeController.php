b9959585b3c1d09b96bbea637762b0a683a92c08
Laravel Daily <info@laraveldaily.com>
2021-05-29 04:10:55 +0000

    QuickAdminPanel automatic commit

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -9,23 +9,96 @@ class HomeController
     public function index()
     {
         $settings1 = [
-            'chart_title'        => 'Report',
-            'chart_type'         => 'line',
-            'report_type'        => 'group_by_relationship',
-            'model'              => 'App\Models\Order',
-            'group_by_field'     => 'name',
-            'aggregate_function' => 'sum',
-            'aggregate_field'    => 'total',
-            'filter_field'       => 'created_at',
-            'filter_days'        => '30',
-            'column_class'       => 'col-md-12',
-            'entries_number'     => '5',
-            'relationship_name'  => 'store',
-            'translation_key'    => 'order',
+            'chart_title'           => 'Total No Of Pharmacies',
+            'chart_type'            => 'number_block',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Pharmacy',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'group_by_field_format' => 'd/m/Y H:i:s',
+            'column_class'          => 'col-md-6',
+            'entries_number'        => '5',
+            'translation_key'       => 'pharmacy',
         ];
 
-        $chart1 = new LaravelChart($settings1);
+        $settings1['total_number'] = 0;
+        if (class_exists($settings1['model'])) {
+            $settings1['total_number'] = $settings1['model']::when(isset($settings1['filter_field']), function ($query) use ($settings1) {
+                if (isset($settings1['filter_days'])) {
+                    return $query->where($settings1['filter_field'], '>=',
+                now()->subDays($settings1['filter_days'])->format('Y-m-d'));
+                }
+                if (isset($settings1['filter_period'])) {
+                    switch ($settings1['filter_period']) {
+                case 'week': $start = date('Y-m-d', strtotime('last Monday')); break;
+                case 'month': $start = date('Y-m') . '-01'; break;
+                case 'year': $start = date('Y') . '-01-01'; break;
+            }
+                    if (isset($start)) {
+                        return $query->where($settings1['filter_field'], '>=', $start);
+                    }
+                }
+            })
+                ->{$settings1['aggregate_function'] ?? 'count'}($settings1['aggregate_field'] ?? '*');
+        }
 
-        return view('home', compact('chart1'));
+        $settings2 = [
+            'chart_title'           => 'Last Thirty Days Orders',
+            'chart_type'            => 'number_block',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Order',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'filter_days'           => '30',
+            'group_by_field_format' => 'd/m/Y H:i:s',
+            'column_class'          => 'col-md-6',
+            'entries_number'        => '5',
+            'translation_key'       => 'order',
+        ];
+
+        $settings2['total_number'] = 0;
+        if (class_exists($settings2['model'])) {
+            $settings2['total_number'] = $settings2['model']::when(isset($settings2['filter_field']), function ($query) use ($settings2) {
+                if (isset($settings2['filter_days'])) {
+                    return $query->where($settings2['filter_field'], '>=',
+                now()->subDays($settings2['filter_days'])->format('Y-m-d'));
+                }
+                if (isset($settings2['filter_period'])) {
+                    switch ($settings2['filter_period']) {
+                case 'week': $start = date('Y-m-d', strtotime('last Monday')); break;
+                case 'month': $start = date('Y-m') . '-01'; break;
+                case 'year': $start = date('Y') . '-01-01'; break;
+            }
+                    if (isset($start)) {
+                        return $query->where($settings2['filter_field'], '>=', $start);
+                    }
+                }
+            })
+                ->{$settings2['aggregate_function'] ?? 'count'}($settings2['aggregate_field'] ?? '*');
+        }
+
+        $settings3 = [
+            'chart_title'           => 'Last Three Months Order Chart',
+            'chart_type'            => 'bar',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Order',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'week',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'filter_days'           => '90',
+            'group_by_field_format' => 'd/m/Y H:i:s',
+            'column_class'          => 'col-md-12',
+            'entries_number'        => '5',
+            'translation_key'       => 'order',
+        ];
+
+        $chart3 = new LaravelChart($settings3);
+
+        return view('home', compact('settings1', 'settings2', 'chart3'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,9 +16,27 @@
                     @endif
 
                     <div class="row">
-                        <div class="{{ $chart1->options['column_class'] }}">
-                            <h3>{!! $chart1->options['chart_title'] !!}</h3>
-                            {!! $chart1->renderHtml() !!}
+                        <div class="{{ $settings1['column_class'] }}">
+                            <div class="card text-white bg-primary">
+                                <div class="card-body pb-0">
+                                    <div class="text-value">{{ number_format($settings1['total_number']) }}</div>
+                                    <div>{{ $settings1['chart_title'] }}</div>
+                                    <br />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="{{ $settings2['column_class'] }}">
+                            <div class="card text-white bg-primary">
+                                <div class="card-body pb-0">
+                                    <div class="text-value">{{ number_format($settings2['total_number']) }}</div>
+                                    <div>{{ $settings2['chart_title'] }}</div>
+                                    <br />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="{{ $chart3->options['column_class'] }}">
+                            <h3>{!! $chart3->options['chart_title'] !!}</h3>
+                            {!! $chart3->renderHtml() !!}
                         </div>
                     </div>
                 </div>
@@ -29,5 +47,5 @@
 @endsection
 @section('scripts')
 @parent
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>{!! $chart1->renderJs() !!}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>{!! $chart3->renderJs() !!}
 @endsection
